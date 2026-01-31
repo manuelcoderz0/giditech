5386f369301275883c460b92fafe0a4851ba46cf
Manuel <manuelcoderz0@gmail.com>
2026-01-31 19:03:17 +0100

    Remove duplicate search route definition in web.php

--- core/routes/web.php
+++ core/routes/web.php
@@ -4,9 +4,10 @@ use Illuminate\Support\Facades\Route;
 
 Route::controller('SiteController')->group(function () {  
     Route::get('/', 'index')->name('home');
+    Route::get('search', 'search')->name('search');
     Route::get('category/{slug}', 'category_details')->name('category.details');
     Route::get('/{slug}', 'post_details')->name('post.details');
-    Route::get('search', 'search')->name('search');
+
     Route::get('placeholder-image/{size}', 'placeholder_image')->name('placeholder.image');
     //Route::get('/{slug}', 'pages')->name('pages');
     Route::get('/contact', 'contact')->name('contact');
